b72f6efeb021f2aec70f5d6e869a4f6781258ee7
GUSTAVO <gus.fatarelli@gmail.com>
2026-03-07 19:06:02 -0300

    Tela de Login, precisa de ajustes na criação do formulário.

---
+++ app/src/main/java/br/com/fiap/doafacil/screens/LoginScreen.kt
@@ -0,0 +1,107 @@
+package br.com.fiap.doafacil.screens
+
+
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.fiap.doafacil.R
+import br.com.fiap.doafacil.ui.theme.DarkBlue
+import br.com.fiap.doafacil.ui.theme.DoafacilTheme
+
+
+
+@Composable
+fun LoginScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+                .align(Alignment.TopEnd),
+            shape = RoundedCornerShape(bottomStart = 16.dp, bottomEnd = 16.dp),
+            colors = CardDefaults
+                .cardColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                )
+        ) {
+
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(R.drawable.logo_doafacil),
+                contentDescription = "",
+                modifier = Modifier
+                    .width(300.dp)
+                    .padding(top = 50.dp)
+            )
+            Text(
+                text = "Doa Fácil",
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.primary,
+                fontWeight = FontWeight.Bold,
+            )
+            Text(
+                text = "Doe com propósito. \nImpacte de verdade.",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.bodyLarge,
+                color = DarkBlue,
+            )
+            Image(
+                painter = painterResource(R.drawable.pessoas_doafacil),
+                contentDescription = "",
+            )
+
+        }
+    }
+}
+
+
+
+@Composable
+@Preview(showBackground = true,
+    showSystemUi = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+
+fun InitialScreenPreview(){
+    DoafacilTheme {
+        LoginScreen()
+    }
+}
