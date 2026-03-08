77d68163eec0fa163c1c405a7b708f6f0965c125
GUSTAVO <gus.fatarelli@gmail.com>
2026-03-08 09:53:43 -0300

    Tela de principal do projeto

---
+++ app/src/main/java/br/com/fiap/doafacil/MainActivity.kt
@@ -0,0 +1,22 @@
+package br.com.fiap.doafacil
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import br.com.fiap.doafacil.screens.LoginScreen
+import br.com.fiap.doafacil.ui.theme.DoafacilTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DoafacilTheme {
+                LoginScreen()
+                }
+            }
+        }
+    }
+
+
